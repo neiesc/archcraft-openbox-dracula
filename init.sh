c580bc2bf047c2c2181c8259399040cd698aa6c8
Edinei <hi@edinei.dev>
2023-08-21 20:45:01 -0300

    fix path ln

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-DOTFILES_FOLDER=$HOME/Dev/Pessoal/dotfiles # if you need set this
 
 echo "Setting theme dracula..."
 
-ln -sf $DOTFILES_FOLDER/files/openbox-themes/themes/dracula $HOME/.config/openbox-themes/themes
+ln -sf $HOME/Dev/Pessoal/archcraft-openbox-dracula/files/openbox-themes/themes/dracula $HOME/.config/openbox-themes/themes
