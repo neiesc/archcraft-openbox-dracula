5483a38916a042b56981b4f11c88c6b4c5c2713a
Edinei <hi@edinei.dev>
2023-08-21 20:25:12 -0300

    reset to default

--- files/openbox-themes/themes/dracula/polybar/scripts/bluetooth.sh
+++ files/openbox-themes/themes/dracula/polybar/scripts/bluetooth.sh
@@ -3,8 +3,10 @@
 ## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
 
 # Colors
-POWER_ON=`cat $HOME/.config/openbox-themes/themes/default/polybar/colors.ini | grep 'GREEN' | head -n1 | cut -d '=' -f2 | tr -d ' '`
-POWER_OFF=`cat $HOME/.config/openbox-themes/themes/default/polybar/colors.ini | grep 'ALTFOREGROUND' | head -n1 | cut -d '=' -f2 | tr -d ' '`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CDIR=`cd "$DIR" && cd .. && pwd`
+POWER_ON=`cat $CDIR/colors.ini | grep 'GREEN' | head -n1 | cut -d '=' -f2 | tr -d ' '`
+POWER_OFF=`cat $CDIR/colors.ini | grep 'ALTFOREGROUND' | head -n1 | cut -d '=' -f2 | tr -d ' '`
 
 # Checks if bluetooth controller is powered on
 power_on() {
