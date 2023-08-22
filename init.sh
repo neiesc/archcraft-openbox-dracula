25e4a92ab15d56334c3211329b2ae93e4ab2f6d4
Edinei <hi@edinei.dev>
2023-08-22 09:36:27 -0300

    add wallpaper

--- init.sh
+++ init.sh
@@ -11,3 +11,5 @@ ln -sf $PWD/dependences/theme/xfce4-terminal/Dracula.theme $HOME/.local/share/xf
 ln -sf $PWD/dependences/theme/gitk/gitk $HOME/.config/git/gitk
 
 ln -sf $PWD/files/openbox-themes/themes/dracula $HOME/.config/openbox-themes/themes
+
+ln -sf $PWD/dependences/theme/wallpaper/second-collection/illustrations/galaxy/dracula-galaxy-282a36.png $PWD/files/openbox-themes/themes/dracula/wallpaper
